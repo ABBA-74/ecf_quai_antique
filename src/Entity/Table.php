a8bdeaa1d8d5062a331c63565484b94df8be04d5
Abdel Bailoul <ab.bailoul@gmail.com>
2023-03-07 18:00:24 +0100

    created Table entity

---
+++ src/Entity/Table.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TableRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TableRepository::class)]
+#[ORM\Table(name: '`table`')]
+class Table
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $number = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $qtyMax = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getQtyMax(): ?int
+    {
+        return $this->qtyMax;
+    }
+
+    public function setQtyMax(int $qtyMax): self
+    {
+        $this->qtyMax = $qtyMax;
+
+        return $this;
+    }
+}
